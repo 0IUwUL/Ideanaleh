047ad1107777cf88cc3c3b8002888af5373deb34
richard <rasunga30@gmail.com>
2022-11-02 21:12:48 +0800

    feat: added models and migrations from db schema

---
+++ database/migrations/2022_11_02_123953_create_tiers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tiers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('proj_id')->unsigned();
+            $table->foreign('proj_id')
+                ->references('id')->on('projects')
+                ->onDelete('cascade');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->string('name');
+            $table->integer('level');
+            $table->integer('amount');
+            $table->string('benefit');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tiers');
+    }
+};

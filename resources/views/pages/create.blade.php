6a5ab9e65b2d25f983e07c00b1f0c30dc12e7a29
0IUwUL <markaljonreales.com>
2022-11-07 01:02:16 +0800

    feat: added other data input

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -17,22 +17,26 @@
                     <textarea class="form-control" placeholder="Short project description..." name = "ProjDesc" id="floatingDesc" style="height: 150px" required></textarea>
                     <label for="floatingDesc">Project Description</label>
                 </div>
-                <div class="row my-5 d-flex justify-content-around">
-                    <div class="col-4">
-                        <label for="floatingTarget">Target Amount</label>
+                <div class="row my-5 d-flex justify-content-start">
+                    <div class="col-5">
+                        <label for="floatingTarget">Target Project Amount</label>
                         <input type="number" class = "form-control form-control-lg" id="floatingTarget" min="100" name = "ProjTarget" required>
                     </div>
                     <div class="col-5">
+                        <label for="floatingTarget">Target Milestone Amount</label>
+                        <input type="number" class = "form-control form-control-lg" id="floatingTarget" min="100" name = "ProjTarget" required>
+                    </div>
+                </div>
+                <div class="row my-5 d-flex justify-content-start">
+                    <div class="col-5 mb-3">
                         <label for="formFile" class="form-label">Upload Project banner</label>
                         <input class="form-control" name = "ProjBanner" type="file" id="formFile" required>
                     </div>
-                </div>
-                <div class="row my-5 d-flex justify-content-around">
                     <div class="col-4">
                         <label for="formFile" class="form-label">Upload Image Logo</label>
                         <input class="form-control" name = "ProjLogo"  type="file" id="formFile">
                     </div>
-                    <div class="col-5">
+                    <div class="col-5 mt-3">
                         <label for="formFile" class="form-label">Link Project Video Promo</label>
                         <input class="form-control" name = "ProjVideo"  type="file" id="formFile">
                     </div>
@@ -40,6 +44,30 @@
                 <div class="col-4 mb-5">
                     <label for="floatingDate">Project Target Date</label>
                     <input type="date" class = "form-control form-control-lg" id="floatingDate" name = "ProjDate" required>
+                </div>
+                <div class="col-8 my-3">
+                    <label>Tier List Titles</label>
+                    <div class="input-group mb-3">
+                        <div class="input-group-text">
+                            <input class="form-check-input mt-0" type="checkbox" value="" id = "FirstTier" aria-label="Checkbox for following text input">
+                            <label for="FirstTier" class = "mx-3">1st Tier</label>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Text input with checkbox">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-text">
+                            <input class="form-check-input mt-0" type="checkbox" value="" id = "SecondTier" aria-label="Checkbox for following text input">
+                            <label for="SecondTier" class = "mx-3">2nd Tier</label>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Text input with checkbox">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-text">
+                            <input class="form-check-input mt-0" type="checkbox" value="" id = "ThirdTier" aria-label="Checkbox for following text input">
+                            <label for="ThirdTier" class = "mx-3">3rd Tier</label>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Text input with checkbox">
+                    </div>
                 </div>
                 <div class="form-floating mb-3 col-6">
                     <textarea class="form-control" placeholder="Eg: #AI, #Agriculture, #IT..." name = "ProjTags" id="floatingTags" style="height: 100px" required></textarea>

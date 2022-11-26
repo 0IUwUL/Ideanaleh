62dfe1cc9623d27bd3bc250618d62a4f852a9a2f
richard <rasunga30@gmail.com>
2022-11-26 15:37:55 +0800

    feat: added ajax for creating project progress

--- resources/views/components/project/view/progress.blade.php
+++ resources/views/components/project/view/progress.blade.php
@@ -1,46 +1,34 @@
 <div class="tab-pane fade" id="pills-update" role="tabpanel" aria-labelledby="pills-update-tab">
     <button type="button" class="btn btn-success mx-lg-5 my-3" data-bs-toggle="modal" data-bs-target="#updateModal">Create update</button>
-    @if (empty($progress))
-        <!-- No progress accordion -->
-        <div class="accordion accordion-flush pb-5 px-lg-5" id="info-update-accordion">
-            <div class="accordion-item-container py-2 px-0 px-sm-2 px-md-3 px-lg-5">
-                <div class="accordion-item">
-                    <div id="info-update-acord-current" class="accordion-collapse collapse show"
-                        aria-labelledby="update-acord-current" data-bs-parent="#info-update-accordion">
-                        <div class="accordion-body py-2 text-center">
-                            <h3> No progress peee peee puuu puuuu</h3>
-                            <!-- by RamonDevs -->
-                        </div>
+    
+    <!-- Current progress accordion -->
+    <div class="accordion accordion-flush pb-5 px-lg-5" id="info-update-accordion">
+        <div class="accordion-item-container py-2 px-0 px-sm-2 px-md-3 px-lg-5">
+            <h2 class="font-weight-bold">Current Progress</h2>
+            <div class="accordion-item" id="accordion-current-progress">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#info-update-acord-current" aria-expanded="true" aria-controls="info-update-acord-1">
+                    <h4 class="accordion-header" id="progress-current-title">
+                        {{empty($progress) ?  'Project started' :
+                        $progress[0]['title'].' ('.date('F d, Y', strtotime($progress[0]['created_at'])).')' }}
+                    </h4>
+                </button>
+                <div id="info-update-acord-current" class="accordion-collapse collapse show"
+                    aria-labelledby="update-acord-current" data-bs-parent="#info-update-accordion">
+                    <div class="accordion-body py-2" id="progress-current-description">              
+                        {{empty($progress) ?  'The developer has just started the project' :
+                        $progress[0]['description'] }}
                     </div>
                 </div>
-            </div> 
-        </div>
-    @else
-        <!-- Current progress accordion -->
-        <div class="accordion accordion-flush pb-5 px-lg-5" id="info-update-accordion">
-            <div class="accordion-item-container py-2 px-0 px-sm-2 px-md-3 px-lg-5">
-                <h2 class="font-weight-bold">Current Progress</h2>
-                <div class="accordion-item">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#info-update-acord-current" aria-expanded="true" aria-controls="info-update-acord-1">
-                        <h4 class="accordion-header" id="update-acord-current">
-                            {{$progress[0]['title']}} ({{date('F d, Y', strtotime($progress[0]['created_at']))}})
-                        </h4>
-                    </button>
-                    <div id="info-update-acord-current" class="accordion-collapse collapse show"
-                        aria-labelledby="update-acord-current" data-bs-parent="#info-update-accordion">
-                        <div class="accordion-body py-2">
-                            <p>{{$progress[0]['description']}}</p>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-        </div>
-        @if(count($progress) > 1)
-        <!-- Progress list accordion -->
-        <div class="accordion accordion-flush px-lg-5" id="info-progress-accordion">
-            <div class="accordion-item-container py-2 px-0 px-sm-2 px-md-3 px-lg-5  mb-4">
-                <h2 class="font-weight-bold">Progress List</h2>
+            </div>
+        </div> 
+    </div>
+    @if(count($progress) > 1)
+    <!-- Progress list accordion -->
+    <div class="accordion accordion-flush px-lg-5" id="info-progress-accordion">
+        <div class="accordion-item-container py-2 px-0 px-sm-2 px-md-3 px-lg-5  mb-4">
+            <h2 class="font-weight-bold">Progress List</h2>
+            <div id = "previous-progress-accordion" >
                 {{-- Get only 5 progress and break if empty --}}
                 @for ($i = 1; $i <=5 ; $i++)
                     @php
@@ -61,11 +49,11 @@
                         </div>
                     </div>
                 @endfor
-                
             </div>
         </div>
-        @endif
+    </div>
     @endif
+    
 </div>
 
 <!-- Modal-->
@@ -77,21 +65,19 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body p-4">
-            <form method="post" action="{{ route('progress.create') }}" accept-charset="UTF-8" id = "updateForm">
+            <form method="post" action="{{ route('progress.create') }}" accept-charset="UTF-8" id = "progressForm">
                 @csrf
                 <input type="hidden" name = "ProjectId" value = "{{$id}}"/>
                 <div class="mb-3">
-                    <label for="InputEmail" class="form-label">Title</label>
+                    <label for="ProgressTitle" class="form-label">Title</label>
                     <input type="text" name = "ProgressTitle" class="form-control " id="progressTitle" required/>
-                    <span class = "error" id = "err_mail"></span>
                 </div>
                 <div class="mb-3">
-                    <label for="InputEmail" class="form-label">Description</label>
+                    <label for="ProgressDesc" class="form-label">Description</label>
                     <textarea name = "ProgressDesc" class="form-control h-75" id="progressDesc" required></textarea>
-                    <span class = "error" id = "err_mail"></span>
                 </div>
             </div>
-            <button type="submit" id="updateSubmit" class="btn btn-primary mx-4 mb-4">Post</button>
+            <button type="button" id="progressSubmit" class="btn btn-primary mx-4 mb-4">Post</button>
         </form>
       </div>
     </div>

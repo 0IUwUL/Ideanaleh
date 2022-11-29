54e4765ef4cc1ed1cc415b21bddc929ebe546941
richard <rasunga30@gmail.com>
2022-11-29 19:10:47 +0800

    feat: added project comment frontend

--- resources/js/project.js
+++ resources/js/project.js
@@ -209,3 +209,14 @@ $("#progressSubmit").click(function(){
     });
   }
 });
+
+// Adjus textarea size
+$("#comment-box").on('keydown', function(){
+  var el = this;
+  setTimeout(function(){
+    el.style.cssText = 'padding:0';
+    // for box-sizing other than "content-box" use:
+    el.style.cssText = '-moz-box-sizing:content-box';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  },0);
+});
--- resources/views/components/project/view/comments.blade.php
+++ resources/views/components/project/view/comments.blade.php
@@ -1,6 +1,32 @@
 <div class="tab-pane fade" id="pills-comments" role="tabpanel" aria-labelledby="pills-comments-tab">
-    <div class="py-5 px-lg-5" id="info-comments-accordion">
-        <!-- Information -->
-        <p class="text-center "> comments </p>
+    <div class="container-md">
+        <div class="col-6 mx-auto my-2 comment"> 
+            <div class="row flex-column">
+                <div class="px-4 py-2 col fw-bold">
+                    <img class="avatar mr-2" src="">
+                    User Name  
+                </div>
+               
+
+                <div class="col px-4 py-2">
+                    <textarea name = "ProgressDesc" class="form-control" maxlength="200" id="comment-box" placeholder="Write a comment..."required></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-column justify-content-center">
+            <div class="col-6 mx-auto my-2 comment"> 
+                <div class="row flex-column">
+                    <div class="px-4 py-2 col fw-bold">
+                        <img class="avatar mr-2" src="">
+                        User Name  
+                    </div>
+                   
+                    <div class="col px-4 py-2">
+                        Comment
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </div>

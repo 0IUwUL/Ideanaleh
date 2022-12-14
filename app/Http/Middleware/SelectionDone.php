b061b2e789e3644bc10c43abc63fb988af71b545
0IUwUL <markaljonreales.com>
2022-12-14 14:49:21 +0800

    feat: added middleware when user registers through google

---
+++ app/Http/Middleware/SelectionDone.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\UserPreference; 
+use App\Models\User; 
+
+class SelectionDone
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) {
+           if ($this->done())
+                return $next($request);
+            return redirect('/');
+        }
+        return redirect('/');
+    }
+
+    private function done(){
+        $user_id = Auth::id();
+        $prefer = (UserPreference::select('followed')->where('user_id', '=', $user_id)->first());
+        $categories = (User::select('pref_categs')->where('id', '=', $user_id)->first());
+
+        if ($prefer->followed && $categories->pref_categs)
+            return true;
+        return false;
+    }
+}

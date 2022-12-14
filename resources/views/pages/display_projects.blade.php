cf1bf32c614473f64190109c4c0cc453144ce23f
unknown <markaisha10@gmail.com>
2022-12-14 12:37:53 +0800

    feat: added lazy loading for filter page images

--- resources/views/pages/display_projects.blade.php
+++ resources/views/pages/display_projects.blade.php
@@ -33,17 +33,17 @@
     <hr>
     <div class="container mt-5">
         <div class="row g-5" id="content_projects">
-            @foreach($ProjArg['projects'] as $project)
+            @foreach($ProjArg['projects'] as $key => $project)
                 <div class="col-md-6">
                     <a href={{ url('project/view/'.$project['id']) }} role = "button">
                         <div class="card h-100">
                             <div class="card-img">
-                                    <img src={{asset('storage/'.$project['banner'])}} class="card-img-top" alt="project_banner">
+                                    <img src={{asset('storage/'.$project['banner'])}} class="card-img-top" alt="project_banner" {{$key > 1 ? 'loading = lazy' : ''}}>
                             </div>
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-12 col-lg-6 title_img">
-                                            <img src={{asset('storage/'.$project['logo'])}} class="mx-auto" alt="project_logo">
+                                            <img src={{asset('storage/'.$project['logo'])}} class="mx-auto" alt="project_logo" {{$key > 1 ? 'loading = lazy' : ''}}>
                                         <h5 class="card-title d-flex justify-content-center text-center mt-5">{{$project['title']}}</h5>
                                     </div>
                                     <div class="col-12 col-lg-6 content">

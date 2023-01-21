22ed612c0f6fe4de62d5ec704f2148a48395d60d
unknown <janbenjaminmesina@gmail.com>
2023-01-21 19:51:15 +0800

    added privacy policy

--- app/Http/Controllers/PolicyController.php
+++ app/Http/Controllers/PolicyController.php
@@ -10,4 +10,7 @@ class PolicyController extends Controller
     public function termsConditions(){
         return view('pages.termsConditions');
     }
+    public function privacyPolicy(){
+        return view('pages.privacyPolicy');
+    }
 }

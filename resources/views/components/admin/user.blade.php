03ab3dbd8c4682b2f74825fe8b10d531e6761559
0IUwUL <markaljonreales.com>
2023-01-31 20:58:11 +0800

    feat: added admin frontend search input

--- resources/views/components/admin/user.blade.php
+++ resources/views/components/admin/user.blade.php
@@ -1,5 +1,18 @@
 <div class="container-fluid mt-5">
     <label for="TableUser" class="admin_user_table">User Table</label>
+    <div class="row py-3 col col-sm-6">
+        <form method="post" action="" accept-charset="UTF-8" class="container-fluid" role="search">
+            @csrf
+            <div class="input-group">
+                <input type="text" class="form-control" name="" value = "" placeholder="Search" id="" aria-label="Search" aria-describedby="" required>
+                <button type="submit" class="input-group-text">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </button>
+            </div>
+        </form>
+    </div>
     <div class="row admin_table" id = "admin_user_table">
         <table class="table table-responsive align-middle table-hover">
             <thead class="table-dark sticky-top">
@@ -61,6 +74,19 @@
         </table>
     </div>
     <label for="TableUserIssue" class="admin_user_table mt-5">User Issue Table</label>
+    <div class="row py-3 col col-sm-6">
+        <form method="post" action="" accept-charset="UTF-8" class="container-fluid" role="search">
+            @csrf
+            <div class="input-group">
+                <input type="text" class="form-control" name="" value = "" placeholder="Search" id="" aria-label="Search" aria-describedby="" required>
+                <button type="submit" class="input-group-text">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </button>
+            </div>
+        </form>
+    </div>
     <div class="row table-responsive mt-3 px-3 admin_table">
         <table class="table align-middle table-hover">
             <thead class="table-dark sticky-top">

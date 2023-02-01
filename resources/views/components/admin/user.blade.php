24cdc5197e3d6ffa7790896dbcc0d912a876a79a
richard <rasunga30@gmail.com>
2023-02-02 00:26:23 +0800

    fix: bug where other admin can disable other admin

--- resources/views/components/admin/user.blade.php
+++ resources/views/components/admin/user.blade.php
@@ -60,11 +60,16 @@
                         <td class="admin_type admin_{{$role}} {{$user['active'] ? 'Active': 'Deactivated'}}">{{ucfirst($role)}}</td>
                         <td>{{date('n/j/Y', strtotime($user['created_at']))}}</td>
                         <td><span class="admin_status {{$role.' admin_'}}{{$user['active'] ? 'active': 'deactive'}}" >{{$user['active'] ? 'Active' : 'Deactivated' }}</span></td> 
-                        <td><button type="button" class="changeStatus btn btn-outline-{{$user['active'] ? 'danger' : 'success'}}" data-bs-toggle="modal" data-bs-target="#ChangeStatusModal" data-id="{{$user['id']}}">
-                                <i class="fa-solid fa-circle-{{$user['active'] ? 'xmark' : 'check'}}"></i>
-                            </button>
-                        </td>
-                        <td><button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#DeleteModal" data-id=""><i class="fa-solid fa-trash-arrow-up"></i></button></td>
+                        @if($role == 'admin')
+                            <td></td>
+                            <td></td>
+                        @else 
+                            <td><button type="button" class="changeStatus btn btn-outline-{{$user['active'] ? 'danger' : 'success'}}" data-bs-toggle="modal" data-bs-target="#ChangeStatusModal" data-id="{{$user['id']}}">
+                                    <i class="fa-solid fa-circle-{{$user['active'] ? 'xmark' : 'check'}}"></i>
+                                </button>
+                            </td>
+                            <td><button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#DeleteModal" data-id=""><i class="fa-solid fa-trash-arrow-up"></i></button></td>
+                        @endif
                     </tr>
                 @endforeach
             </tbody>
